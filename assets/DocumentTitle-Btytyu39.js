ba18fb6bb4a1049075a7c7519e7818703caa81a3
kunish <kunish@users.noreply.github.com>
2024-11-24 10:20:42 +0000

    v1.173.0 f6103337175fa28fa7334cf4937a9f1daf85c542

---
+++ assets/DocumentTitle-Btytyu39.js
@@ -0,0 +1,1 @@
+import{d as t,bC as r}from"./index-8p10MaTT.js";const o=({children:e})=>t(r,{get children(){return[e," - MetaCubeXD"]}});export{o as D};
---
+++ assets/time-DuOqfXk_.js
@@ -0,0 +1,1 @@
+import{W as a,b4 as m}from"./index-8p10MaTT.js";const s=o=>a(o).locale(m()).fromNow();export{s as f};
